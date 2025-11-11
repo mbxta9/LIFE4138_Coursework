f691fac674c2aaf77d8f395234a947e5cc94c60c
mbxta9 <235346231+mbxta9@users.noreply.github.com>
2025-11-11 16:47:26 +0000

    Updating answers

--- r_testing.r
+++ r_testing.r
@@ -13,3 +13,35 @@ my_list <- list(my_vector, my_matrix, my_dataframe)
 
 my_vector[4]
 my_matrix[7, 4]
+my_dataframe[10, "name"]
+my_list[[1]][5]
+
+# Question 3
+# Method 1 - print
+print(200:300)
+
+# Method 2 - for loop
+for (i in 200:300) {
+    print(i)
+}
+
+# Method 3 - sapply
+values <- 200:300
+values
+sapply(values, print)
+
+# Question 4
+animals <- c("tiger", "lion", "badger", "fox", "rabbit", "fish", "dog", "octopus")
+animals <- replace(animals, c(6, 8), c("squirrel", "horse"))
+animals
+
+# Question 5
+data(cars)
+plot(cars, xlab = "Speed (mph)", ylab = "Stopping distance (ft)", main = "Scatterplot of car stopping distances vs speed")
+
+# Question 6
+install.packages("dplyr")
+library(dplyr)
+data(starwars)
+head(arrange(starwars, desc(height)), 1)
+as.da
