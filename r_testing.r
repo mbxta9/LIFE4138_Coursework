1cc697fdfd03cc8936a562753d5867f97c61ff53
mbxta9 <235346231+mbxta9@users.noreply.github.com>
2025-11-11 15:17:00 +0000

    Adding Testing files

---
+++ r_testing.r
@@ -0,0 +1,14 @@
+# Question 1
+df <- data.frame(
+    People = c("Tahir", "Jiaan", "Caleb", "Layla", "Chris"),
+    Height = c(160, 170, 185, 140, 160)
+)
+head(df)
+
+# Question 2
+my_vector <- seq(from = 1, to = 1000, by = 1)
+my_matrix <- matrix(1:100, nrow = 10, ncol = 10)
+my_dataframe <- data.frame(name = rep(c("a", "b"), each = 10), value = 21:40)
+my_list <- list(my_vector, my_matrix, my_dataframe)
+
+my_vector[4]
