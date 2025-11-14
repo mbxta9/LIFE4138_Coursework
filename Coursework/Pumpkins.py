9edc570d847b7810c89c5b9ad37767d47ca15c11
mbxta9 <235346231+mbxta9@users.noreply.github.com>
2025-11-14 14:23:14 +0000

    Starting pumpkins script

---
+++ Coursework/Pumpkins.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+#Importing requirements
+import pandas as pd
+
+#Importing student dataset
+
+def import_dataset(path :str): 
+    '''
+    Function to import a csv dataset from its filepath or url
+    Checks to see if 
+    Arguments:
+    path: Takes a string of a url or filepath of a csv file to import.
+
+    Returns:
+    dataset - returns a pandas dataframe
+    '''
+    try:
+        dataset = pd.read_csv(path)
+        return dataset
+    except FileNotFoundError:
+        print("File not found, please enter a valid path")
+
+
+def find_highest(dataframe: pd,column: str):
+    '''
+    Function to get the row information of the highest value in a column
+
+    Arguments:
+    dataframe: the dataframe to search through
+    column: the column to search through for the highest value
+    '''
+    try:
+        highest_row = dataframe.loc[dataframe[column].idxmax()]
+        return highest_row
+    except KeyError:
+        print("Please use a valid column name")
+    except NameError:
+        print('Please provide a valid variable')
+    except AttributeError:
+        print('Please enter a valid argument type')
+
+
+
+
+def main():
+    '''
+    Main script to run analysis
+    '''
+    #Importing dataset
+    pumpkins = import_dataset('Coursework/Pumpkins/pumpkins_datasets/pumpkins_02.csv')
+
+    #Finding heaviest pumpkin
+    heaviest_pumpkin = find_highest(pumpkins,'weight_lbs')
+    print(f"The heaviest pumpkin was a {heaviest_pumpkin['variety']} from {heaviest_pumpkin['city']}, {heaviest_pumpkin['state_prov']}, {heaviest_pumpkin['country']}. It weighed {round(heaviest_pumpkin['weight_lbs'],2)}lbs and was grown in the year {heaviest_pumpkin['id']}")
+
+if __name__ == '__main__': #Ensures script runs
+    main()
