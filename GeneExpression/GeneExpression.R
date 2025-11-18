5b4d6f4b7ae5e27fd06a294a2e240a58a51e699e
mbxta9 <235346231+mbxta9@users.noreply.github.com>
2025-11-18 17:05:55 +0000

    Adding Volcano plots

--- GeneExpression/GeneExpression.R
+++ GeneExpression/GeneExpression.R
@@ -1,5 +1,3 @@
-#!/usr/bin/Rscript
-
 ####################################################################
 #                                                                  #
 #        Title: Gene Expression R Script                           #
@@ -14,11 +12,15 @@
 
 
 # Installing necessary packages
-# install.packages("tidyverse")
+#install.packages("tidyverse")
 #install.packages("DT")
+#install.packages("knitr")
 library(tidyverse)
 library(DT)
 library(knitr)
+library(ggrepel)
+library(RColorBrewer)
+library(plotly)
 
 
 # Loading the datasets
@@ -30,12 +32,12 @@ a_vs_d <- read_tsv("Datasets/set_2/A_vs_D.deseq2.results.tsv")
 
 ## Number of significantly upregulated genes
 find_sig_upreg <- function(dataset) {
-    changed <- subset(dataset, log2FoldChange >= 1 & padj < 0.05)
+    changed <- subset(dataset, log2FoldChange >= 0.5 & padj < 0.05)
     return(changed)
 }
 ## Number of significantly downregulated genes
 find_sig_downreg <- function(dataset) {
-    changed <- subset(dataset, log2FoldChange <= -1 & padj < 0.05)
+    changed <- subset(dataset, log2FoldChange <= -0.5 & padj < 0.05)
     return(changed)
 }
 
@@ -50,22 +52,68 @@ up_down <- data.frame(Dataset_Name=c('A vs B','A vs D'),
 kable(up_down)
 
 #Table of summary stats
+generate_summary <- function(dataset,column) {
+    summary <- c(
+        min(dataset[[column]],na.rm = TRUE),
+        max(dataset[[column]],na.rm = TRUE),
+        mean(dataset[[column]],na.rm = TRUE),
+        median(dataset[[column]],na.rm = TRUE),
+        quantile(dataset[[column]],0.25, na.rm = TRUE),
+        quantile(dataset[[column]],0.75, na.rm = TRUE)
+    )
+    return(summary)
+}
+
+#A vs B table
 stat_names = c('Min','Max','Mean','Median','Lower Quartile', 'Upper Quartile')
-p_value <- c(
-    min(a_vs_b[["pvalue"]],na.rm = TRUE),
-    max(a_vs_b[["pvalue"]],na.rm = TRUE),
-    mean(a_vs_b[["pvalue"]],na.rm = TRUE),
-    median(a_vs_b[["pvalue"]],na.rm = TRUE),
-    quantile(a_vs_b[["pvalue"]],0.25, na.rm = TRUE),
-    quantile(a_vs_b[["pvalue"]],0.75, na.rm = TRUE)
-    )
-log2fold <- c(
-    min(a_vs_b[["log2FoldChange"]],na.rm = TRUE),
-    max(a_vs_b[["log2FoldChange"]],na.rm = TRUE),
-    mean(a_vs_b[["log2FoldChange"]],na.rm = TRUE),
-    median(a_vs_b[["log2FoldChange"]],na.rm = TRUE),
-    quantile(a_vs_b[["log2FoldChange"]],0.25, na.rm = TRUE),
-    quantile(a_vs_b[["log2FoldChange"]],0.75, na.rm = TRUE)
-    )
-output_a_b <- data.frame(stat_names, p_value, log2fold)
-kable(output_a_b)
+p_value <- generate_summary(a_vs_b,'pvalue')
+log2fold <- generate_summary(a_vs_b,'log2FoldChange')
+output_a_b <- data.frame(stat_names,p_value,log2fold)
+kable(output_a_b, col.names=c("Statistic","P Value","Log2FoldChange"),caption = "Table 1: Summary statistics on the A vs B Dataset")
+
+#A vs D table
+p_value <- generate_summary(a_vs_d,'pvalue')
+log2fold <- generate_summary(a_vs_d,'log2FoldChange')
+output_a_d <- data.frame(stat_names,p_value,log2fold)
+kable(output_a_d, col.names=c("Statistic","P Value","Log2FoldChange"),caption = "Table 1: Summary statistics on the A vs D Dataset")
+
+#Volcano plot
+#Creating a new column for colouring
+a_vs_b$diffexpressed <- "NO"
+a_vs_b$diffexpressed[a_vs_b$log2FoldChange >= 0.5 & a_vs_b$pvalue < 0.05] <- "UP"
+a_vs_b$diffexpressed[a_vs_b$log2FoldChange <= -0.5 & a_vs_b$pvalue < 0.05] <- "DOWN"
+
+#Creating the volcano plot
+a_vs_b_volcano <- ggplot(data = a_vs_b, aes(x = log2FoldChange, y = -log10(pvalue), color = diffexpressed, text = gene_id)) +
+    geom_point() +
+    scale_color_manual(values = c("red", "grey", "blue"), labels = c('Downregulated', 'Not Significant', 'Upregulated')) +
+    coord_cartesian(ylim = c(0, 20), xlim = c(-5, 5)) +
+    labs(
+        x = "Log 2 Fold Change",
+        y = "-log10(pvalue)",
+        title = "Volcano plot of A vs B",
+        color = "Significance"
+    ) +
+    theme_light() +
+    theme(plot.title = element_text(hjust = 0.5))
+ggplotly(a_vs_b_volcano)
+
+#Creating a new column for colouring
+a_vs_d$diffexpressed <- "NO"
+a_vs_d$diffexpressed[a_vs_d$log2FoldChange >= 0.5 & a_vs_d$pvalue < 0.05] <- "UP"
+a_vs_d$diffexpressed[a_vs_d$log2FoldChange <= -0.5 & a_vs_d$pvalue < 0.05] <- "DOWN"
+
+#Creating the volcano plot
+a_vs_d_volcano <- ggplot(data = a_vs_d, aes(x = log2FoldChange, y = -log10(pvalue), color = diffexpressed, text = gene_id)) +
+    geom_point() +
+    scale_color_manual(values = c("red", "grey", "blue"), labels = c('Downregulated', 'Not Significant', 'Upregulated')) +
+    coord_cartesian(ylim = c(0, 20), xlim = c(-10, 10)) +
+    labs(
+        x = "Log 2 Fold Change",
+        y = "-log10(pvalue)",
+        title = "Volcano plot of A vs D",
+        color = "Significance"
+    ) +
+    theme_light() +
+    theme(plot.title = element_text(hjust = 0.5))
+ggplotly(a_vs_d_volcano)
